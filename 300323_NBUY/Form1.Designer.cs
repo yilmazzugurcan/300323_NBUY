a2d8c0e95acf39ffd8913188e187d659d0aa9454
yilma <yilma@DESKTOP-U1LH654>
2023-03-30 21:27:06 +0300

    Add project files.

---
+++ 300323_NBUY/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace _300323_NBUY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbYiyecek = new System.Windows.Forms.ComboBox();
+            this.cmbCesit = new System.Windows.Forms.ComboBox();
+            this.lstYiyeceks = new System.Windows.Forms.ListView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbYiyecek
+            // 
+            this.cmbYiyecek.FormattingEnabled = true;
+            this.cmbYiyecek.Items.AddRange(new object[] {
+            "Yiyecek",
+            "İçecek"});
+            this.cmbYiyecek.Location = new System.Drawing.Point(32, 30);
+            this.cmbYiyecek.Name = "cmbYiyecek";
+            this.cmbYiyecek.Size = new System.Drawing.Size(225, 21);
+            this.cmbYiyecek.TabIndex = 0;
+            this.cmbYiyecek.SelectedIndexChanged += new System.EventHandler(this.cmbYiyecek_SelectedIndexChanged);
+            // 
+            // cmbCesit
+            // 
+            this.cmbCesit.FormattingEnabled = true;
+            this.cmbCesit.Location = new System.Drawing.Point(32, 72);
+            this.cmbCesit.Name = "cmbCesit";
+            this.cmbCesit.Size = new System.Drawing.Size(225, 21);
+            this.cmbCesit.TabIndex = 2;
+            this.cmbCesit.SelectedIndexChanged += new System.EventHandler(this.cmbCesit_SelectedIndexChanged);
+            // 
+            // lstYiyeceks
+            // 
+            this.lstYiyeceks.HideSelection = false;
+            this.lstYiyeceks.Location = new System.Drawing.Point(32, 121);
+            this.lstYiyeceks.Name = "lstYiyeceks";
+            this.lstYiyeceks.Size = new System.Drawing.Size(225, 283);
+            this.lstYiyeceks.TabIndex = 3;
+            this.lstYiyeceks.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(297, 30);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(141, 23);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "Siparişe Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lstYiyeceks);
+            this.Controls.Add(this.cmbCesit);
+            this.Controls.Add(this.cmbYiyecek);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbYiyecek;
+        private System.Windows.Forms.ComboBox cmbCesit;
+        private System.Windows.Forms.ListView lstYiyeceks;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
---
+++ 300323_NBUY/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _300323_NBUY
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        
+        }
+        int selectedFoodIndex = -1;
+        int selectedTypeIndex = -1;
+
+        private void cmbYiyecek_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string cesit = cmbYiyecek.SelectedItem.ToString();
+           
+            if (cesit == "Yiyecek")
+            {
+                cmbCesit.Items.Clear();
+                cmbCesit.Items.Add("Hamburger");
+                cmbCesit.Items.Add("Cheesburger");
+                cmbCesit.Items.Add("Karışık Pizza");
+                cmbCesit.Items.Add("Sucuklu Pizza");
+                cmbCesit.Items.Add("Ton Balıklı Pizza");
+               
+
+            }
+            else if (cesit == "İçecek")
+            {
+                cmbCesit.Items.Clear();
+                cmbCesit.Items.Add("Su");
+                cmbCesit.Items.Add("Ayran");
+                cmbCesit.Items.Add("Kola");
+                cmbCesit.Items.Add("Gazoz");
+                cmbCesit.Items.Add("Ice Tea");
+               
+            }
+            
+        }
+        
+        private void cmbCesit_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+    }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string yiyecek = cmbCesit.SelectedItem.ToString();
+            string cesit = cmbCesit.SelectedItem.ToString();
+            string fiyat = null;
+
+            if (yiyecek == "Yiyecek")
+            {
+                if (cesit == "Hamburger")
+                    fiyat = "20 TL";
+                else if (cesit == "Cheesburger")
+                    fiyat = "25 TL";
+                else if (cesit == "Karışık Pizza")
+                    fiyat = "30 TL";
+                else if (cesit == "Sucuklu Pizza")
+                    fiyat = "35 TL";
+                else if (cesit == "Ton Balıklı Pizza")
+                    fiyat = "40 TL";
+            }
+            else if (yiyecek == "İçecek")
+            {
+                if (cesit == "Su")
+                    fiyat = "2 TL";
+                else if (cesit == "Ayran")
+                    fiyat = "3 TL";
+                else if (cesit == "Kola")
+                    fiyat = "5 TL";
+                else if (cesit == "Gazoz")
+                    fiyat = "5 TL";
+                else if (cesit == "Ice Tea")
+                    fiyat = "7 TL";
+            }
+
+            ListViewItem lvi = new ListViewItem();
+            lvi.Text = yiyecek;
+            lvi.SubItems.Add(cesit);
+            lvi.SubItems.Add(fiyat);
+            lstYiyeceks.Items.Add(lvi);
+
+        }
+    }
+}
